6113b2918b007f197f3da2e6cfe117a8caf72ea9
yisj777s <yisj777s@naver.com>
2024-09-08 19:02:37 +0900

    unfinished02 02.리액트 동작 원리_02-2.JSX 구문 이해하기

--- ch02/ch02_2/src/index.tsx
+++ ch02/ch02_2/src/index.tsx
@@ -1,14 +1,24 @@
 import ReactDOM from 'react-dom/client'
 
-const rootVirtualDOM = (
-  <ul>
-    <li>
-      <a href="http://www.google.com" target="_blank">
-        <p>go to Google</p>
-      </a>
-    </li>
-  </ul>
-)
+const children = [
+  <li>
+    <a href="http://www.google.com" target="_blank">
+      <p>go to Google</p>
+    </a>
+  </li>,
+  <li>
+    <a href="http://www.facebook.com" target="_blank">
+      <p>go to Facebook</p>
+    </a>
+  </li>,
+  <li>
+    <a href="http://www.twitter.com" target="_blank">
+      <p>go to Twitter</p>
+    </a>
+  </li>
+]
+
+const rootVirtualDOM = <ul>{children}</ul>
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(rootVirtualDOM)
