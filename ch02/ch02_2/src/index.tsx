02684df574cedd23e5aa74b6592ec5e7649eb0e2
yisj777s <yisj777s@naver.com>
2024-09-09 19:06:36 +0900

    add 02.리액트 동작 원리_02-2.JSX 구문 이해하기

--- ch02/ch02_2/src/index.tsx
+++ ch02/ch02_2/src/index.tsx
@@ -1,24 +1,16 @@
 import ReactDOM from 'react-dom/client'
+import * as D from './data'
 
-const children = [
-  <li>
-    <a href="http://www.google.com" target="_blank">
-      <p>go to Google</p>
-    </a>
-  </li>,
-  <li>
-    <a href="http://www.facebook.com" target="_blank">
-      <p>go to Facebook</p>
-    </a>
-  </li>,
-  <li>
-    <a href="http://www.twitter.com" target="_blank">
-      <p>go to Twitter</p>
-    </a>
-  </li>
-]
-
-const rootVirtualDOM = <ul>{children}</ul>
+const children = D.makeArray(10).map((notUsed, index) => (
+  <div key={index}>
+    <p>{D.randomId()}</p>
+    <p>{D.randomName()}</p>
+    <p>{D.randomJobTitle()}</p>
+    <p>{D.randomSentence()}</p>
+    <img src={D.randomAvatar()} width={100} height={100} />
+  </div>
+))
+const rootVirtualDOM = <div>{children}</div>
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(rootVirtualDOM)
