e0d64b6be2ebc60236390423d5b73e7b7c9e79d3
yisj777s <yisj777s@naver.com>
2024-09-10 19:24:43 +0900

    unfinished01 02.리액트 동작 원리_02-3.컴포넌트 이해하기

---
+++ ch02/ch02_3/src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <ul>
+      <li>
+        <a href="http://www.google.com">
+          <p>go to Google</p>
+        </a>
+      </li>
+    </ul>
+  )
+}
