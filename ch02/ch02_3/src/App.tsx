5dd6350c07c30e3196b53309fbae5fb1366652c5
yisj777s <yisj777s@naver.com>
2024-09-11 19:21:43 +0900

    unfinished02 02.리액트 동작 원리_02-3.컴포넌트 이해하기

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,11 +1,19 @@
-export default function App() {
-  return (
-    <ul>
-      <li>
-        <a href="http://www.google.com">
-          <p>go to Google</p>
-        </a>
-      </li>
-    </ul>
-  )
+import {Component} from 'react'
+
+export default class App extends Component {
+  render() {
+    const isLoading = true
+    const children = isLoading ? (
+      <p>loading...</p>
+    ) : (
+      <ul>
+        <li>
+          <a href="http://www.google.com">
+            <p>go to Google</p>
+          </a>
+        </li>
+      </ul>
+    )
+    return <div>{children}</div>
+  }
 }
