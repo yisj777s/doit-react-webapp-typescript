43d483f197dde5f1ad9df45af506574f9a9cb224
yisj777s <yisj777s@naver.com>
2024-09-13 19:57:36 +0900

    add 02.리액트 동작 원리_02-3.컴포넌트 이해하기

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,13 +1,11 @@
-import {Component} from 'react'
 import ClassComponent from './ClassComponent'
+import ArrowComponent from './ArrowComponent'
 
-export default class App extends Component {
-  render() {
-    return (
-      <ul>
-        <ClassComponent href="http://www.google.com" text="go to Google" />
-        <ClassComponent href="https://twitter.com" text="go to Twitter" />
-      </ul>
-    )
-  }
+export default function App() {
+  return (
+    <ul>
+      <ClassComponent href="http://www.google.com" text="go to Google" />
+      <ArrowComponent href="https://twitter.com" text="go to Twitter" />
+    </ul>
+  )
 }
