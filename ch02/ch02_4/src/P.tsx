9bfaeb0694c5a9b765b9988761ddf82f270af06c
yisj777s <yisj777s@naver.com>
2024-09-15 18:38:28 +0900

    add 02.리액트 동작 원리_02-4.Key와 children 속성 이해하기

--- ch02/ch02_4/src/P.tsx
+++ ch02/ch02_4/src/P.tsx
@@ -1,10 +1,7 @@
-import type {FC, ReactNode} from 'react'
+import type {FC, PropsWithChildren} from 'react'
 
-export type PProps = {
-  children?: ReactNode
-}
-const P: FC<PProps> = props => {
-  const {children} = props
-  return <p children={children} />
+export type PProps = {}
+const P: FC<PropsWithChildren<PProps>> = props => {
+  return <p {...props} />
 }
 export default P
