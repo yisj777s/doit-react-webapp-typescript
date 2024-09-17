d07e88273fd7ef4d53e81facf16460ab4b3b6d53
yisj777s <yisj777s@naver.com>
2024-09-17 22:33:20 +0900

    unfinished02 02.리액트 동작 원리_02-5.이벤트 속성 이해하기

--- ch02/ch02_5/src/App.tsx
+++ ch02/ch02_5/src/App.tsx
@@ -1,9 +1,29 @@
-import CopyMe from './copy/CopyMe'
+import EventListener from './pages/EventListener'
+import OnClick from './pages/OnClick'
+import ReactOnClick from './pages/ReactOnClick'
+import DispatchEvent from './pages/DispatchEvent'
+import EventBubbling from './pages/EventBubbling'
+import StopPropagation from './pages/StopPropagation'
+import VariousInputs from './pages/VariousInputs'
+import OnChange from './pages/OnChange'
+import FileInput from './pages/FileInput'
+import DragDrop from './pages/DragDrop'
+import FileDrop from './pages/FileDrop'
 
 export default function App() {
   return (
-    <h1>
-      <CopyMe />
-    </h1>
+    <div>
+      <FileDrop />
+      <DragDrop />
+      <FileInput />
+      <OnChange />
+      <VariousInputs />
+      <StopPropagation />
+      <EventBubbling />
+      <DispatchEvent />
+      <ReactOnClick />
+      <OnClick />
+      <EventListener />
+    </div>
   )
 }
