8cc8824cbbeecaa62771e71c16b51bda761bec2c
yisj777s <yisj777s@naver.com>
2024-09-22 21:00:51 +0900

    unfinished07 02.리액트 동작 원리_02-5.이벤트 속성 이해하기

--- ch02/ch02_5/src/App.tsx
+++ ch02/ch02_5/src/App.tsx
@@ -17,8 +17,8 @@ export default function App() {
       <DragDrop />
       <FileInput />
       <OnChange />
-      <VariousInputs />
-      {/* <StopPropagation />
+      {/* <VariousInputs />
+      <StopPropagation />
       <EventBubbling />
       <DispatchEvent />
       <ReactOnClick />
