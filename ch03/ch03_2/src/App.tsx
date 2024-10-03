3a3868d44a5f4a468679b51e7774e0f984ff8874
yisj777s <yisj777s@naver.com>
2024-10-03 18:44:28 +0900

    unfinished02 03.컴포넌트 CSS 스타일링_03_2.테일윈드CSS 리액트 프로젝트 만들기

--- ch03/ch03_2/src/App.tsx
+++ ch03/ch03_2/src/App.tsx
@@ -1,18 +1,13 @@
-import Bootstrap from './pages/Bootstrap'
-import Icon from './pages/Icon'
-import Style from './pages/Style'
-import UsingIcon from './pages/UsingIcon'
-import UsingIconWithCSSClass from './pages/UsingIconWithCSSClass'
-import './App.css'
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextsTest'
 
 export default function App() {
   return (
     <div>
-      <UsingIconWithCSSClass />
-      <UsingIcon />
-      <Style />
-      {/* <Icon />
-      <Bootstrap /> */}
+      <TextsTest />
+      <Color />
+      <Tailwindcss />
     </div>
   )
 }
