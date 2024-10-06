e287227805c8ec96f974176923727bcff4bbd102
yisj777s <yisj777s@naver.com>
2024-10-06 22:06:02 +0900

    unfinished05 03.컴포넌트 CSS 스타일링_03_2.테일윈드CSS 리액트 프로젝트 만들기

--- ch03/ch03_2/src/pages/Color.tsx
+++ ch03/ch03_2/src/pages/Color.tsx
@@ -1,3 +1,12 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+export default function Color() {
+  return (
+    <div className="p-4 bg-sky-700">
+      <p className="w-full p-4 text-3xl text-white">Color</p>
+      <div className="mb-4">
+        <p className="text-white">Email address</p>
+        <input type="email" className="text-gray-900 border-4 border-sky-200" />
+        <p className="text-rose-500">This field is required</p>
+      </div>
+    </div>
+  )
 }
