dc0826d77ed26c5574f0ef362f28e973ba2e1a64
yisj777s <yisj777s@naver.com>
2024-10-10 21:40:58 +0900

    unfinished09 03.컴포넌트 CSS 스타일링_03_2.테일윈드CSS 리액트 프로젝트 만들기

--- ch03/ch03_2/src/pages/TextsTest.tsx
+++ ch03/ch03_2/src/pages/TextsTest.tsx
@@ -1,3 +1,23 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import * as D from '../data'
+import {Title, Subtitle, Summary, Paragraph} from '../components'
+
+const paragraphs = D.makeArray(2).map(D.randomParagraphs).join('\n\n')
+const summery = D.makeArray(3).map(D.randomSentence).join('\n')
+
+export default function TextsTest() {
+  return (
+    <div>
+      <Title>TextsTest</Title>
+      <div>
+        <Title className="text-blue-600">{D.randomTitleText()}</Title>
+        <Subtitle className="text-blue-400">{D.randomSentence()}</Subtitle>
+        <p className="text-xl italic font-bold text-center text-gray-900">
+          {D.randomName()}
+        </p>
+        <Paragraph numberOfLines={5}>{paragraphs}</Paragraph>
+        <Summary className="text-center text-gray-500">{summery}</Summary>
+        <p className="text-center text-p">{D.randomDay</p>
+      </div>
+    </div>
+  )
 }
