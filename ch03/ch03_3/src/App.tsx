15d86a1fd8bf18eeb42fd32bc5cbfdb53d4e815b
yisj777s <yisj777s@naver.com>
2024-10-12 21:34:41 +0900

    unfinished02 03.컴포넌트 CSS 스타일링_03_3.CSS 상자 모델 이해하기

--- ch03/ch03_3/src/App.tsx
+++ ch03/ch03_3/src/App.tsx
@@ -1,13 +1,31 @@
-import Tailwindcss from './pages/Tailwindcss'
-import Color from './pages/Color'
-import TextsTest from './pages/TextsTest'
+import DivTest from './pages/DivTest'
+import ViewportTest from './pages/ViewportTest'
+import HeightTest from './pages/HeightTest'
+import PaddingTest from './pages/PaddingTest'
+import MarginTest from './pages/MarginTest'
+import ImageTest from './pages/ImageTest'
+import BackgroundImageTest from './pages/BackgroundImageTest'
+import DisplayTest from './pages/DisplayTest'
+import DisplayNoneTest from './pages/DisplayNoneTest'
+import AvatarTest from './pages/AvatarTest'
+import PositionTest from './pages/PositionTest'
+import OverlayTest from './pages/OverlayTest'
 
 export default function App() {
   return (
-    <div>
-      <TextsTest />
-      <Color />
-      <Tailwindcss />
-    </div>
+    <main>
+      <OverlayTest />
+      <PositionTest />
+      <DisplayNoneTest />
+      <DisplayTest />
+      <AvatarTest />
+      <BackgroundImageTest />
+      <ImageTest />
+      <MarginTest />
+      <PaddingTest />
+      <HeightTest />
+      <ViewportTest />
+      <DivTest />
+    </main>
   )
 }
