4f75fac688252bab60a9f3a572ea2db8859f8ad6
yisj777s <yisj777s@naver.com>
2024-10-15 22:04:50 +0900

    unfinished05 03.컴포넌트 CSS 스타일링_03_3.CSS 상자 모델 이해하기

--- ch03/ch03_3/src/pages/DivTest.tsx
+++ ch03/ch03_3/src/pages/DivTest.tsx
@@ -4,7 +4,8 @@ export default function DivTest() {
   return (
     <section className="mt-4">
       <Title>DivTest</Title>
-      <Div className="text-center text-blue-100 bg-blue-600" height="6rem">
+      {/* <Div className="text-center text-blue-100 bg-blue-600" height="6rem"> */}
+      <Div className="text-center text-blue-100 bg-blue-600">
         <Icon name="home" className="text-3xl"></Icon>
         <Subtitle>Home</Subtitle>
       </Div>
