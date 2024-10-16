48425f85ecc27161e1a676b55e867819fbfbacf7
yisj777s <yisj777s@naver.com>
2024-10-16 21:54:59 +0900

    unfinished06 03.컴포넌트 CSS 스타일링_03_3.CSS 상자 모델 이해하기

--- ch03/ch03_3/src/pages/ViewportTest.tsx
+++ ch03/ch03_3/src/pages/ViewportTest.tsx
@@ -1,10 +1,9 @@
 import {Title} from '../components'
 
-export default function CopyMe() {
+export default function ViewportTest() {
   return (
-    <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+    <section className="w-screen h-screen mt-4 bg-indigo-900">
+      <Title className="text-white">ViewportTest</Title>
     </section>
   )
 }
