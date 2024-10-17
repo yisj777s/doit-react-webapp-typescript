8618325d1874f922d8b7a2f5afd9bd67a12c5d99
yisj777s <yisj777s@naver.com>
2024-10-17 22:21:32 +0900

    unfinished07 03.컴포넌트 CSS 스타일링_03_3.CSS 상자 모델 이해하기

--- ch03/ch03_3/src/pages/HeightTest.tsx
+++ ch03/ch03_3/src/pages/HeightTest.tsx
@@ -1,10 +1,17 @@
-import {Title} from '../components'
+import {Title, Div} from '../components'
 
-export default function CopyMe() {
+export default function HeightTest() {
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>HeightTest</Title>
+      <Div className="h-40 mt-4 text-center bg-blue-500">
+        <Div className="bg-blue-500 h-1/2">
+          <p className="text-center text-red-50">h-1/2</p>
+        </Div>
+        <Div className="h-20 bg-red-500">
+          <p className="text-center text-red-50">h-20</p>
+        </Div>
+      </Div>
     </section>
   )
 }
--- ch03/ch03_3/src/pages/PaddingTest.tsx
+++ ch03/ch03_3/src/pages/PaddingTest.tsx
@@ -1,10 +1,19 @@
 import {Title} from '../components'
+import * as D from '../data'
 
-export default function CopyMe() {
+const sentence = D.randomSentence(10)
+export default function PaddingTest() {
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>PaddingTest</Title>
+      <div className="p-8">
+        <div className="text-white bg-sky-600">
+          <p>{sentence}</p>
+        </div>
+        <div className="p-8 text-white bg-orange-600">
+          <p>{sentence}</p>
+        </div>
+      </div>
     </section>
   )
 }
