92f022484bc82231de5a1b5a9878f783c76b6d37
yisj777s <yisj777s@naver.com>
2024-10-18 22:20:08 +0900

    unfinished08 03.컴포넌트 CSS 스타일링_03_3.CSS 상자 모델 이해하기

--- ch03/ch03_3/src/pages/MarginTest.tsx
+++ ch03/ch03_3/src/pages/MarginTest.tsx
@@ -1,10 +1,15 @@
 import {Title} from '../components'
+import * as D from '../data'
 
-export default function CopyMe() {
+export default function MarginTest() {
+  const boxes = D.makeArray(10).map((notUsed, index) => (
+    <div key={index} className="inline-block w-8 h-8 m-4 bg-pink-300" />
+  ))
+
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>MarginTest</Title>
+      <div className="p-4 bg-blue-700">{boxes}</div>
     </section>
   )
 }
