388879e8b55869148f607b498181e8c909e35498
yisj777s <yisj777s@naver.com>
2024-10-20 21:28:32 +0900

    unfinished10 03.컴포넌트 CSS 스타일링_03_3.CSS 상자 모델 이해하기

--- ch03/ch03_3/src/pages/BackgroundImageTest.tsx
+++ ch03/ch03_3/src/pages/BackgroundImageTest.tsx
@@ -1,10 +1,14 @@
-import {Title} from '../components'
+import {Div, Title, Subtitle} from '../components'
+import * as D from '../data'
 
-export default function CopyMe() {
+const src = D.randomImage(1200, 400)
+export default function BackgroundImageTest() {
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>BackgroundImageTest</Title>
+      <Div className="mt-4 bg-gray-300 h-80" src={src}>
+        <Subtitle>Some Text here</Subtitle>
+      </Div>
     </section>
   )
 }
