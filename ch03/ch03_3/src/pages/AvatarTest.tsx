801002efc7f41654edb4ec23005b6e86fc8c98df
yisj777s <yisj777s@naver.com>
2024-10-22 19:52:31 +0900

    unfinished12 03.컴포넌트 CSS 스타일링_03_3.CSS 상자 모델 이해하기

--- ch03/ch03_3/src/pages/AvatarTest.tsx
+++ ch03/ch03_3/src/pages/AvatarTest.tsx
@@ -1,10 +1,18 @@
-import {Title} from '../components'
+import {Div, Title, Avatar} from '../components'
+import * as D from '../data'
 
-export default function CopyMe() {
+export default function AvatarTest() {
+  const avatars = D.range(0, 10).map(index => (
+    <Avatar
+      className="inline-block -ml-6 border-4 border-white"
+      key={index}
+      src={D.randomAvatar()}
+    />
+  ))
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>AvatarTest</Title>
+      <Div className="px-12 py-4 m-8 bg-blue-300">{avatars}</Div>
     </section>
   )
 }
