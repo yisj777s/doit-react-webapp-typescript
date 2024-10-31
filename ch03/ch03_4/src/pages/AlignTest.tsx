c3c0ddd43107c5d7acce2512404c77a3a57347c8
yisj777s <yisj777s@naver.com>
2024-10-31 22:25:17 +0900

    unfinished05 03.컴포넌트 CSS 스타일링_03-4.플렉스 레이아웃 이해하기

--- ch03/ch03_4/src/pages/AlignTest.tsx
+++ ch03/ch03_4/src/pages/AlignTest.tsx
@@ -1,10 +1,28 @@
-import {Title} from '../components'
+import {Div, Title, Subtitle} from '../components'
+import * as D from '../data'
 
-export default function CopyMe() {
+export default function AlignTest() {
+  const boxes = D.range(0, 5).map(index => {
+    return <div key={index} className="w-4 h-4 m-1 bg-black" />
+  })
+  const boxesForStretch = D.range(0, 10).map(index => {
+    return <div key={index} className="w-4 m-4 bg-black" />
+  })
+  const justifies = ['dd']
+  const justifies = ['ddd', 'ss', '']
+  // prettier-ignore
+  const justifies = []
   return (
-    <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+    <section className="p-4 mt-4">
+      <Title>JustifyCenterTest</Title>
+      <div className="mt-4">
+        <Subtitle>flex flex-row justify-center</Subtitle>
+        <div className="flex flex-row justify-center h-40 bg-">{boxes}</div>
+      </div>
+      <div className="mt-4">
+        <Subtitle></Subtitle>
+        <div>{boxes}</div>
+      </div>
     </section>
   )
 }
