08e900b09e17574d22c7adef362d6ba89e02cf8d
yisj777s <yisj777s@naver.com>
2024-11-03 21:59:47 +0900

    unfinished08 03.컴포넌트 CSS 스타일링_03-4.플렉스 레이아웃 이해하기

--- ch03/ch03_4/src/pages/UserContainer.tsx
+++ ch03/ch03_4/src/pages/UserContainer.tsx
@@ -3,12 +3,23 @@ import * as D from '../data'
 import User from './User'
 
 export default function UserContainer() {
-  const children = D.makeArray(10).map(D.makeRandomUser)
-
+  const children = D.makeArray(10)
+    .map(D.makeRandomUser)
+    .map(user => (
+      <User
+        key={user.uuid}
+        user={user}
+        className="m-2 text-xs border-2 border-blue-300 rounded-lg"
+        minWidth="15rem"
+        width="15rem"
+      />
+    ))
   return (
-    <section>
+    <section className="mt-4">
       <Title>UserContainer</Title>
-      <div></div>
+      <div className="flex flex-wrap items-center justify-center p-4 mt-4">
+        {children}
+      </div>
     </section>
   )
 }
