12e7df9770a9b348a53ba5714738967c4cea0d2d
yisj777s <yisj777s@naver.com>
2024-11-05 23:40:13 +0900

    unfinished10 03.컴포넌트 CSS 스타일링_03-4.플렉스 레이아웃 이해하기

--- ch03/ch03_4/src/pages/Card.tsx
+++ ch03/ch03_4/src/pages/Card.tsx
@@ -3,3 +3,18 @@ import type {DivProps} from '../components'
 import {Div} from '../components'
 import * as D from '../data'
 import User from './User'
+
+export type CardProps = DivProps & {
+  card: D.ICard
+}
+const Card: FC<CardProps> = ({card, ...props}) => {
+  const {writer, image, title, paragraphs, dayMonthYearDate, relativeDate} = card
+  return (
+    <Div {...props}>
+      <div>
+        <User user={writer} className="mt-2" />
+      </div>
+    </Div>
+  )
+}
+export default Card
