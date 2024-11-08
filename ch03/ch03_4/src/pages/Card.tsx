77853196b5de2832a388f9ead5a826bc8cbe35de
yisj777s <yisj777s@naver.com>
2024-11-08 22:03:03 +0900

    unfinished13 03.컴포넌트 CSS 스타일링_03-4.플렉스 레이아웃 이해하기

--- ch03/ch03_4/src/pages/Card.tsx
+++ ch03/ch03_4/src/pages/Card.tsx
@@ -10,11 +10,15 @@ export type CardProps = DivProps & {
 const Card: FC<CardProps> = ({card, ...props}) => {
   console.log(card)
   const {writer, image, title, paragraphs, dayMonthYearDate, relativeDate} = card
+  const icons = ['home', 'search', 'settings', 'favorite'].map(name => (
+    <Icon key={name} name={name} className="mr-20 text-3xl" />
+  ))
   return (
     <Div {...props}>
-      <div>
+      <div className="flex flex-col">
         <Div src={image} className="h-60" />
         <User user={writer} className="mt-2" />
+        <Div>{icons}</Div>
       </div>
     </Div>
   )
