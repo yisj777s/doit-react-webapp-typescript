6e2abde0724efa9787e72f2aa69a53bedcb3787b
yisj777s <yisj777s@naver.com>
2024-11-09 20:34:36 +0900

    add 03.컴포넌트 CSS 스타일링_03-4.플렉스 레이아웃 이해하기

--- ch03/ch03_4/src/pages/Card.tsx
+++ ch03/ch03_4/src/pages/Card.tsx
@@ -13,12 +13,25 @@ const Card: FC<CardProps> = ({card, ...props}) => {
   const icons = ['home', 'search', 'settings', 'favorite'].map(name => (
     <Icon key={name} name={name} className="mr-20 text-3xl" />
   ))
+
   return (
     <Div {...props}>
       <div className="flex flex-col">
         <Div src={image} className="h-60" />
-        <User user={writer} className="mt-2" />
-        <Div>{icons}</Div>
+        <Div className="p-4" minHeight="16rem" height="16rem" maxHeight="16rem">
+          <p className="mt-2 text-3xl font-bold text-center">{title}</p>
+          <Div className="flex justify-between">
+            <User user={writer} className="mt-2" />
+            <Div className="mt-2">
+              <p className="text-gray-500">{relativeDate}</p>
+              <p className="text-gray-500">{dayMonthYearDate}</p>
+            </Div>
+          </Div>
+          <p className="mt-2 line-clamp-4">{paragraphs}</p>
+          <Div className="flex flex-row items-center justify-between p-2 mt-2 text-red-500">
+            {icons}
+          </Div>
+        </Div>
       </div>
     </Div>
   )
