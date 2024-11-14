6bd1a94891caa7ee96ff064b6d09b5752f38910d
yisj777s <yisj777s@naver.com>
2024-11-14 21:43:20 +0900

    unfinished05 03.컴포넌트 CSS 스타일링_03-5.daisyui css 컴포넌트 이해하기

--- ch03/ch03_5/src/pages/ButtonTest/IconTest.tsx
+++ ch03/ch03_5/src/pages/ButtonTest/IconTest.tsx
@@ -1,7 +1,10 @@
-export default function CopyMe() {
+import {Icon} from '../../theme/daisyui'
+//prettier-ignore
+export default function IconTest() {
+  const onClick = () => alert('Icon clicked')
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
+      <h2 className="text-5xl font-bold text-center">IconTest</h2>
       <div className="mt-4"></div>
     </section>
   )
--- ch03/ch03_5/src/theme/daisyui/Icon.tsx
+++ ch03/ch03_5/src/theme/daisyui/Icon.tsx
@@ -8,3 +8,12 @@ export type IconProps = ButtonProps &
   CIconProps & {
     iconClassName?: string
   }
+
+export const Icon: FC<IconProps> = ({name, iconClassName, className, ...buttonProps}) => {
+  const btnClassName = ['btn-circle', className].join(' ')
+  return (
+    <Button {...buttonProps} className={btnClassName}>
+      <CIcon className={iconClassName} name={name} />
+    </Button>
+  )
+}
