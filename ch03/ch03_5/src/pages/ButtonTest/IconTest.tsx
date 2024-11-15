e0d9f0ece789070bf6e9b13d3663dc925e1aab1e
yisj777s <yisj777s@naver.com>
2024-11-15 22:09:25 +0900

    unfinished06 03.컴포넌트 CSS 스타일링_03-5.daisyui css 컴포넌트 이해하기

--- ch03/ch03_5/src/pages/ButtonTest/IconTest.tsx
+++ ch03/ch03_5/src/pages/ButtonTest/IconTest.tsx
@@ -5,7 +5,15 @@ export default function IconTest() {
   return (
     <section className="mt-4">
       <h2 className="text-5xl font-bold text-center">IconTest</h2>
-      <div className="mt-4"></div>
+      <div className="flex items-center justify-around mt-4">
+        <Icon className="btn-primary btn-lg" iconClassName="text-5xl"
+              name="settings" onClick={onClick} />
+        <Icon className="btn-secondary btn-md" iconClassName="text-3xl"
+              name="done" onClick={onClick} />
+        <Icon className="btn-accent btn-sm" iconClassName="text-xl"
+              name="menu" onClick={onClick} />
+        <Icon className="btn-success btn-xs" name="file_upload" onClick={onClick} />  
+      </div>
     </section>
   )
 }
