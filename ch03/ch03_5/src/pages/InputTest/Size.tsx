6b18a3d761c070f0894f65d7eb181df36a73d86d
yisj777s <yisj777s@naver.com>
2024-11-17 23:55:29 +0900

    unfinished08 03.컴포넌트 CSS 스타일링_03-5.daisyui css 컴포넌트 이해하기

--- ch03/ch03_5/src/pages/InputTest/Size.tsx
+++ ch03/ch03_5/src/pages/InputTest/Size.tsx
@@ -1,8 +1,27 @@
-export default function CopyMe() {
+import {Input} from '../../theme/daisyui'
+
+export default function Size() {
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <h2 className="text-3xl font-bold text-center">Size</h2>
+      <div className="flex mt-4 justify-evenly">
+        <div>
+          <label className="label">input-lg</label>
+          <Input className="input-primary input-lg" />
+        </div>
+        <div>
+          <label className="label">input-md</label>
+          <Input className="input-primary input-md" />
+        </div>
+        <div>
+          <label className="label">input-sm</label>
+          <Input className="input-primary input-sm" />
+        </div>
+        <div>
+          <label className="label">input-xs</label>
+          <Input className="input-primary input-xs" />
+        </div>
+      </div>
     </section>
   )
 }
