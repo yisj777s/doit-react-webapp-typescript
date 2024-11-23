3985c2a909eeea140365ac9c9b9ff852456abaf5
yisj777s <yisj777s@naver.com>
2024-11-23 19:05:57 +0900

    unfinished03 04.함수 컴포넌트와 리액트 훅_04-1.처음 만나는 리액트 훅

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,6 +1,15 @@
+import {useEffect} from 'react'
 import Clock from './pages/Clock'
 
 export default function App() {
   let today = new Date()
+  useEffect(() => {
+    console.log('useEffect called.')
+    const duration = 1000
+    const id = setInterval(() => {
+      today = new Date()
+      console.log('today', today.toLocaleTimeString())
+    }, duration)
+  }, [])
   return <Clock today={today} />
 }
