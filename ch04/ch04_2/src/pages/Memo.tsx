b16f35ddff50894041c91aed24824ffcc9558d31
yisj777s <yisj777s@naver.com>
2024-12-02 21:40:15 +0900

    unfinished05 04.함수 컴포넌트와 리액트 훅_04-2.useMemo와 useCallback 훅 이해하기

--- ch04/ch04_2/src/pages/Memo.tsx
+++ ch04/ch04_2/src/pages/Memo.tsx
@@ -1,8 +1,45 @@
-export default function CopyMe() {
+import {Title, Avatar} from '../components'
+import * as D from '../data'
+import {useOrCreate} from './useOrCreate'
+
+export default function CreateOrUseTest() {
+  //prettier-ignore
+  const headTexts = useOrCreate<string[]>('headText', () => [
+    'NO.', 'NAME', 'JOB TITLE', 'EMAIL ADDRESS'
+  ])
+  const users = useOrCreate<D.IUser[]>('users', () =>
+    D.makeArray(100).map(D.makeRandomUser)
+  )
+
+  const head = useOrCreate('head', () =>
+    headTexts.map(text => <th key={text}>{text}</th>)
+  )
+
+  const body = useOrCreate('children', () =>
+    users.map((user, index) => (
+      <tr key={user.uuid}>
+        <th>{index + 1}</th>
+        <td className="flex items-center">
+          <Avatar src={user.avatar} size="1.5rem" />
+          <p className="ml-2">{user.name}</p>
+        </td>
+        <td>{user.jobTitle}</td>
+        <td>{user.email}</td>
+      </tr>
+    ))
+  )
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
+    <div className="mt-4">
+      <Title>CreateOrUseTest</Title>
+      <div className="p-4 mt-4 overflow-x-auto">
+        <table className="table w-full table-zebra table-compact">
+          <thead>
+            <tr>{head}</tr>
+          </thead>
+          <tbody>{body}</tbody>
+        </table>
+      </div>
+    </div>
   )
 }
