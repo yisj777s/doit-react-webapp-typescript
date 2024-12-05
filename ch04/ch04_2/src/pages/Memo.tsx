f8efb8c24eb464e73496ce7e550dd98da400f3f5
yisj777s <yisj777s@naver.com>
2024-12-05 22:33:20 +0900

    unfinished07 04.함수 컴포넌트와 리액트 훅_04-2.useMemo와 useCallback 훅 이해하기

--- ch04/ch04_2/src/pages/Memo.tsx
+++ ch04/ch04_2/src/pages/Memo.tsx
@@ -13,9 +13,33 @@ export default function Memo() {
     () => headTexts.map(text => <th key={text}>{text}</th>),
     [headTexts]
   )
+  //prettier-ignore
+  const body = useMemo(
+    () => 
+      users.map((user, index) => (
+      <tr key={user.uuid}>
+        <th>{index + 1}</th>
+        <td className="flex items-center">
+          <Avatar src={user.avatar} size="1.5rem" />
+          <p className="ml-2">{user.name}</p>
+        </td>
+        <td>{user.jobTitle}</td>
+        <td>{user.email}</td>
+      </tr>)), 
+      [users]
+  )
 
-  console.log('headText =>', headTexts)
-  console.log('head =>', head)
-
-  return <div></div>
+  return (
+    <div className="mt-4">
+      <Title>Memo</Title>
+      <div className="w-full mt-4 overflow-x-auto">
+        <table className="table w-full table-zebra table-compact">
+          <thead>
+            <tr>{head}</tr>
+          </thead>
+          <tbody>{body}</tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
