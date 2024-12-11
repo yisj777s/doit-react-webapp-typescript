a151ae200895a3585ab043ec2d57d6cd278fdedd
yisj777s <yisj777s@naver.com>
2024-12-11 19:19:12 +0900

    unfinished03 04.함수 컴포넌트와 리액트 훅_04-3.useState 훅 이해하기

--- ch04/ch04_3/src/pages/NumberState.tsx
+++ ch04/ch04_3/src/pages/NumberState.tsx
@@ -1,8 +1,25 @@
-export default function CopyMe() {
+import {useState, useCallback} from 'react'
+import {Title} from '../components'
+import {Icon} from '../theme/daisyui'
+
+export default function NumberState() {
+  const [count, setCount] = useState<number>(0)
+
+  const increment = useCallback(() => {
+    setCount(count => count + 1)
+  }, [])
+  const decrement = useCallback(() => {
+    setCount(count => count - 1)
+  }, [])
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+    <section className="mt-4 mb-8">
+      <Title>NumberState</Title>
+      <div className="mt-4">
+        <div>
+          <Icon />
+        </div>
+      </div>
     </section>
   )
 }
