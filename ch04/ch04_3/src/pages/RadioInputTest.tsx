c21bdeefbc0728d4e40c821490a3a96d61a9f216
yisj777s <yisj777s@naver.com>
2024-12-17 23:25:15 +0900

    unfinished08 04.함수 컴포넌트와 리액트 훅_04-3.useState 훅 이해하기

--- ch04/ch04_3/src/pages/RadioInputTest.tsx
+++ ch04/ch04_3/src/pages/RadioInputTest.tsx
@@ -1,4 +1,22 @@
-export default function CopyMe() {
+import {useMemo} from 'react'
+import {Title, Subtitle} from '../components'
+import * as D from '../data'
+
+export default function RadioInputTest() {
+  const jobTitles = useMemo(() => D.makeArray(4).map(D.randomJobTitle), [])
+  const radioInputs = useMemo(() =>
+    jobTitles.map((value, index) => (
+      <label key={index} className="flex justify-start cursor-pointer label">
+        <input
+          type="radio"
+          name="jobs"
+          className="mr-4 radio radio-primary"
+          defaultValue={value}
+        />
+      </label>
+    ))
+  )
+
   return (
     <section className="mt-4">
       <h2 className="text-5xl font-bold text-center">CopyMe</h2>
