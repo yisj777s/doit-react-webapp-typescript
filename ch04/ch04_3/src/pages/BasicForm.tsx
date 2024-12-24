a023edbc7ea8e11fb800b64b2083e9936bdf630c
yisj777s <yisj777s@naver.com>
2024-12-24 23:01:19 +0900

    unfinished13 04.함수 컴포넌트와 리액트 훅_04-3.useState 훅 이해하기

--- ch04/ch04_3/src/pages/BasicForm.tsx
+++ ch04/ch04_3/src/pages/BasicForm.tsx
@@ -30,11 +30,27 @@ export default function BasicForm() {
   return (
     <section className="mt-4">
       <Title>BasicForm</Title>
-      <div className="mt-4">
-        <form >
-          <div></div>
-          <div></div>
-          <div></div>
+      <div className="flex justify-center mt-4">
+        <form onSubmit={onSubmit}>
+          <div className="form-control">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Username</span>
+            </label>
+            <Input value={name} onChange={onChangeName} id="name" type="text"
+              placeholder="enter your name" className="input-primary" />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="email">
+              <span className="label-text">email</span>
+            </label>
+            <Input value={email} onChange={onChangeEmail} id="email" type="email" 
+              placeholder="enter your email" className="input-primary" />
+          </div>
+          <div className="flex justify-center mt-4">
+            <input type="submit" value="SUBMIT"
+              className="w-1/2 btn btn-sm btn-primary"/>
+            <input type="button" defaultValue="CANCEL" className="w-1/2 ml-4 btn btn-sm"/>
+          </div>
         </form>
       </div>
     </section>
