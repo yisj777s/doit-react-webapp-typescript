64a71aca8837fa6949ae2a7a9cf0d440ab593e3d
yisj777s <yisj777s@naver.com>
2024-12-27 19:14:47 +0900

    add 04.함수 컴포넌트와 리액트 훅_04-3.useState 훅 이해하기

--- ch04/ch04_3/src/App.tsx
+++ ch04/ch04_3/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <main>
       <ArrayState />
-      <ObjectState />
+      {/* <ObjectState /> */}
       {/* <BasicForm /> */}
       {/* <HigherOrderRadioInputTest /> */}
       {/* <RadioInputTest /> */}
