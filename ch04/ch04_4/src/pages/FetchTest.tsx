4eedab7a9843bd05f44f7c733b2b78ca89510459
yisj777s <yisj777s@naver.com>
2025-01-09 19:03:01 +0900

    unfinished09 04.함수 컴포넌트와 리액트 훅_04-4.useEffect와 useLayoutEffect 훅 이해하기기

--- ch04/ch04_4/src/pages/FetchTest.tsx
+++ ch04/ch04_4/src/pages/FetchTest.tsx
@@ -1,4 +1,14 @@
-export default function CopyMe() {
+import {useState, useCallback, useEffect} from 'react'
+import {useToggle} from '../hooks'
+import {Title, Avatar, Icon} from '../components'
+import {Button} from '../theme/daisyui'
+import * as D from '../data'
+
+export default function FetchTest() {
+  const [loading, toggleLoading] = useToggle()
+  const [randomUser, setRandomUser] = useState<D.IRandomUser | null>(null)
+  const [error, setError] = useState<Error | null>(null)
+
   return (
     <section className="mt-4">
       <h2 className="text-5xl font-bold text-center">CopyMe</h2>
