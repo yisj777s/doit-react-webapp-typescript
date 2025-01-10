f77b0b3f3243b4a1e2ed2d1eb18ffd0a97cb458c
yisj777s <yisj777s@naver.com>
2025-01-10 19:00:30 +0900

    add 04.함수 컴포넌트와 리액트 훅_04-4.useEffect와 useLayoutEffect 훅 이해하기기

--- ch04/ch04_4/src/App.tsx
+++ ch04/ch04_4/src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <main>
       <FetchTest />
-      <WindowResizeTest />
+      {/* <WindowResizeTest /> */}
       {/* <ClassLifecycle /> */}
     </main>
   )
--- ch04/ch04_4/src/pages/FetchTest.tsx
+++ ch04/ch04_4/src/pages/FetchTest.tsx
@@ -9,10 +9,42 @@ export default function FetchTest() {
   const [randomUser, setRandomUser] = useState<D.IRandomUser | null>(null)
   const [error, setError] = useState<Error | null>(null)
 
+  const getRandomUser = useCallback(() => {
+    toggleLoading()
+    D.fetchRandomUser().then(setRandomUser).catch(setError).finally(toggleLoading)
+  }, [toggleLoading])
+  useEffect(getRandomUser, [getRandomUser])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>FetchTest</Title>
+      <div className="flex justify-center mt-4">
+        <Button className="btn-sm btn-primary" onClick={getRandomUser}>
+          <Icon name="get_app" />
+          <span>get random user</span>
+        </Button>
+      </div>
+      {loading && (
+        <div className="flex items-center justify-center">
+          <Button className="btn-circle loading"></Button>
+        </div>
+      )}
+      {error && (
+        <div className="p-4 mt-4 bg-red-200">
+          <p className="text-3xl text-red-500 text-bold">{error.message}</p>
+        </div>
+      )}
+      {randomUser && (
+        <div className="flex justify-center p-4 mt-4">
+          <Avatar src={randomUser.picture.large} />
+          <div className="ml-4">
+            <p className="text-xl text-bold">
+              {randomUser.name.title}. {randomUser.name.first} {randomUser.name.last}
+            </p>
+            <p className="italic text-gray-600">{randomUser?.email}</p>
+          </div>
+        </div>
+      )}
     </section>
   )
 }
