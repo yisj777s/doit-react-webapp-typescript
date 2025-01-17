4d368a82f621d4f971932f2698d37b22604a6f1d
yisj777s <yisj777s@naver.com>
2025-01-17 19:36:29 +0900

    unfinished05 04.함수 컴포넌트와 리액트 훅_04-5.useRef와 useImperativeHandle 훅 이해하기기

--- ch04/ch04_5/src/pages/FileDrop.tsx
+++ ch04/ch04_5/src/pages/FileDrop.tsx
@@ -2,8 +2,29 @@ import type {ChangeEvent, DragEvent} from 'react'
 import {useState, useRef, useCallback, useMemo} from 'react'
 import {useToggle} from '../hooks'
 import {Title, Div} from '../components'
+import {imageFileReaderP} from '../utils'
+import {Button} from '../theme/daisyui'
 
-export default function CopyMe() {
+export default function FileDrop() {
+  const [imageUrls, setImageUrls] = useState<string[]>([])
+  const [error, setError] = useState<Error | null>(null)
+  const [loading, toggleLoading] = useToggle(false)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const onDivClick = useCallback(() => inputRef.current?.click(), [])
+
+  const makeImageUrls = useCallback(
+    (files: File[]) => {
+      const promises = Array.from(files).map(imageFileReaderP)
+      toggleLoading()
+      Promise.all(promises)
+        .then(urls => setImageUrls(imageUrls => [...urls, ...imageUrls]))
+        .catch(setError)
+        .finally(toggleLoading)
+    },
+    [toggleLoading]
+  )
+
   return (
     <section className="mt-4">
       <h2 className="text-5xl font-bold text-center">CopyMe</h2>
