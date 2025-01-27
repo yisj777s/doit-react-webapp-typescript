6e40ba15892bf9f81f41623cb6d0807868cd844c
yisj777s <yisj777s@naver.com>
2025-01-27 19:30:47 +0900

    unfinished11 04.함수 컴포넌트와 리액트 훅_04-5.useRef와 useImperativeHandle 훅 이해하기기

--- ch04/ch04_5/src/pages/ValidatableInputTest.tsx
+++ ch04/ch04_5/src/pages/ValidatableInputTest.tsx
@@ -1,8 +1,31 @@
-export default function CopyMe() {
+import {useRef, useCallback} from 'react'
+import {Title} from '../components'
+import type {ValidatableInputMethods} from '../theme/daisyui'
+import {ValidatableInput, Button} from '../theme/daisyui'
+
+export default function ValidatableInputTest() {
+  const methodsRef = useRef<ValidatableInputMethods>(null)
+
+  const validateEmail = useCallback(() => {
+    if (methodsRef.current) {
+      const [valid, valueOrErrorMessage] = methodsRef.current.validate()
+      if (valid) alert(`${valueOrErrorMessage}는 유효한 이메일 주소입니다.`)
+      else alert(valueOrErrorMessage)
+    }
+  }, [])
+
+  // prettier-ignore
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ValidatableInputTest</Title>
+      <div className="mt-4">
+        <div>
+          <ValidatableInput type="email" ref={methodsRef} className="input-primary" />
+          <Button onClick={validateEmail}>
+            VALIDATE
+          </Button>
+        </div>
+      </div>
     </section>
   )
 }
