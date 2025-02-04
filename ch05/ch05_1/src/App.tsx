783f28ab1c85a4867a6caa2dac3c12615a2ab9d6
yisj777s <yisj777s@naver.com>
2025-02-04 21:06:43 +0900

    unfinished01 05.상태 관리와 리덕스 패키지_05-1.리덕스 기본 개념 이해하기기

---
+++ ch05/ch05_1/src/App.tsx
@@ -0,0 +1,11 @@
+import ReduxClock from './pages/ReduxClock'
+import UseReducerClock from './pages/UseReducerClock'
+
+export default function App() {
+  return (
+    <main>
+      <UseReducerClock />
+      <ReduxClock />
+    </main>
+  )
+}
