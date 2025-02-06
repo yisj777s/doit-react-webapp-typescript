466d3ff17587d80c63b8825e3664f098a370f048
yisj777s <yisj777s@naver.com>
2025-02-06 22:10:13 +0900

    unfinished03 05.상태 관리와 리덕스 패키지_05-1.리덕스 기본 개념 이해하기기

--- ch05/ch05_1/src/App.tsx
+++ ch05/ch05_1/src/App.tsx
@@ -1,7 +1,16 @@
 import type {Action} from 'redux'
 import {Provider as ReduxProvider} from 'react-redux'
+import {configureStore} from '@reduxjs/toolkit'
 import ReduxClock from './pages/ReduxClock'
 import UseReducerClock from './pages/UseReducerClock'
+
+type AppState = {
+  today: Date
+}
+const initialAppState = {
+  today: new Date()
+}
+const rootReducer = (state: AppState = )
 
 export default function App() {
   return (
