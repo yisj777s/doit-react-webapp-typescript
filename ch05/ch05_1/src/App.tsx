49075e485d199382c333f5ff6b83202c77741842
yisj777s <yisj777s@naver.com>
2025-02-11 22:19:24 +0900

    unfinished06 05.상태 관리와 리덕스 패키지_05-1.리덕스 기본 개념 이해하기기

--- ch05/ch05_1/src/App.tsx
+++ ch05/ch05_1/src/App.tsx
@@ -1,22 +1,11 @@
-import type {Action} from 'redux'
 import {Provider as ReduxProvider} from 'react-redux'
-import {configureStore} from '@reduxjs/toolkit'
+import {useStore} from './store'
+
 import ReduxClock from './pages/ReduxClock'
 import UseReducerClock from './pages/UseReducerClock'
 
-type AppState = {
-  today: Date
-}
-const initialAppState = {
-  today: new Date()
-}
-const rootReducer = (state: AppState = initialAppState, action: Action) => state
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware()
-})
-
 export default function App() {
+  const store = useStore()
   return (
     <ReduxProvider store={store}>
       <main className="p-8">
--- ch05/ch05_1/src/store/useStore.ts
+++ ch05/ch05_1/src/store/useStore.ts
@@ -2,7 +2,15 @@ import {configureStore} from '@reduxjs/toolkit'
 import {useMemo} from 'react'
 import {rootReducer} from './rootReducer'
 
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware()
-})
+const initiallizeStore = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware()
+  })
+  return store
+}
+
+export function useStore() {
+  const store = useMemo(() => initiallizeStore(), [])
+  return store
+}
