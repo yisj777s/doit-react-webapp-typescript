beb320fe78a36f5b4b682492aae3ed8c6f74d073
yisj777s <yisj777s@naver.com>
2025-02-26 19:22:10 +0900

    unfinished02 05.상태 관리와 리덕스 패키지_05-2.리듀서 활용하기

--- ch05/ch05_2/src/store/AppState.ts
+++ ch05/ch05_2/src/store/AppState.ts
@@ -1,1 +1,11 @@
-export type AppState = {}
+import * as Clock from './clock'
+import * as Counter from './counter'
+import * as R from './remoteUser'
+import * as Cards from './cards'
+
+export type AppState = {
+  clock: Clock.State
+  counter: Counter.State
+  remoteUser: R.State
+  cards: Cards.State
+}
--- ch05/ch05_2/src/store/rootReducer.ts
+++ ch05/ch05_2/src/store/rootReducer.ts
@@ -1,2 +1,12 @@
 import {combineReducers} from 'redux'
-export const rootReducer = combineReducers({})
+import * as Clock from './clock'
+import * as Counter from './counter'
+import * as R from './remoteUser'
+import * as Cards from './cards'
+
+export const rootReducer = combineReducers({
+  clock: Clock.reducer,
+  counter: Counter.reducer,
+  remoteUser: R.reducer,
+  cards: Cards.reducer
+})
