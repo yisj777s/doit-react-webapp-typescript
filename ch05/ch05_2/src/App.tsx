ae0eb88a3db4ff18c53d00210410e1e7c3a666c5
yisj777s <yisj777s@naver.com>
2025-02-27 19:57:08 +0900

    unfinished03 05.상태 관리와 리덕스 패키지_05-2.리듀서 활용하기

--- ch05/ch05_2/src/App.tsx
+++ ch05/ch05_2/src/App.tsx
@@ -1,11 +1,18 @@
 import {Provider as ReduxProvider} from 'react-redux'
 import {useStore} from './store'
+import ClockTest from './pages/ClockTest'
+import CounterTest from './pages/CounterTest'
+import RemoteUserTest from './pages/RemoteUserTest'
+import CardTest from './pages/CardsTest'
 
 export default function App() {
   const store = useStore()
   return (
     <ReduxProvider store={store}>
-      <div />
+      <CardTest />
+      <RemoteUserTest />
+      <CounterTest />
+      <ClockTest />
     </ReduxProvider>
   )
 }
--- ch05/ch05_2/src/store/clock/types.ts
+++ ch05/ch05_2/src/store/clock/types.ts
@@ -1,4 +1,9 @@
 import type {Action} from 'redux'
 
-export type State = any
-export type Actions = Action
+export type State = string
+
+export type SetClockActions = Action<'@clock/setClock'> & {
+  payload: State
+}
+
+export type Actions = SetClockActions
