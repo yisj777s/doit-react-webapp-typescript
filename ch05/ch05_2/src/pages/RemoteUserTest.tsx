b01167710248ffe8b39dddba996411b3d623ed36
yisj777s <yisj777s@naver.com>
2025-03-10 22:25:44 +0900

    unfinished11 05.상태 관리와 리덕스 패키지_05-2.리듀서 활용하기

--- ch05/ch05_2/src/pages/RemoteUserTest.tsx
+++ ch05/ch05_2/src/pages/RemoteUserTest.tsx
@@ -1,11 +1,18 @@
 import {useState, useCallback, useEffect} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import {useToggle} from '../hooks'
-import {Title} from '../components'
+import {Title, Avatar} from '../components'
+import {Button} from '../theme/daisyui'
+import * as D from '../data'
+import type {AppState} from '../store'
+import * as R from '../store/remoteUser'
 
-export default function CopyMe() {
+export default function RemoteUserTest() {
+  const dispatch = useDispatch()
+  const user = useSelector<AppState, R.State>(({remoteUser}) => remoteUser)
+
   return (
-    <section className="mb-80">
+    <section className="mb-4">
       <Title>RemoteUserTest</Title>
       <div className="mt-4"></div>
     </section>
