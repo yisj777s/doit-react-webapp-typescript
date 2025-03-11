1d9de7900e47489832395d6cc20a3511b12e7919
yisj777s <yisj777s@naver.com>
2025-03-11 20:43:42 +0900

    unfinished12 05.상태 관리와 리덕스 패키지_05-2.리듀서 활용하기

--- ch05/ch05_2/src/pages/RemoteUserTest.tsx
+++ ch05/ch05_2/src/pages/RemoteUserTest.tsx
@@ -10,6 +10,32 @@ import * as R from '../store/remoteUser'
 export default function RemoteUserTest() {
   const dispatch = useDispatch()
   const user = useSelector<AppState, R.State>(({remoteUser}) => remoteUser)
+  const [loading, toggleLoading] = useToggle()
+  const [error, setError] = useState<Error | null>(null)
+
+  const getRemoteUser = useCallback(() => {
+    toggleLoading()
+    D.fetchRandomUser()
+      .then(user => dispatch(D.setUser(user)))
+      .catch(setError)
+      .finally(toggleLoading)
+  }, [dispatch, toggleLoading])
+  const changeName = useCallback(() => {
+    toggleLoading()
+    D.fetchRandomUser()
+      .then(user => dispatch(R.changeName(user.name)))
+      .catch(setError)
+      .finally(toggleLoading)
+  }, [dispatch, toggleLoading])
+  const changeEmail = useCallback(
+    () => dispatch(R.changeEmail(D.randomEmail())),
+    [dispatch]
+  )
+  const changePicture = useCallback(() =>
+    dispatch(R.changePicture({large: D.randomAvatar()}), [dispatch])
+  )
+
+  useEffect(getRemoteUser, [getRemoteUser])
 
   return (
     <section className="mb-4">
