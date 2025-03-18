5d899476479becda0a274b4a08ab4a6d59c171ff
yisj777s <yisj777s@naver.com>
2025-03-18 21:00:25 +0900

    unfinished17 05.상태 관리와 리덕스 패키지_05-2.리듀서 활용하기

---
+++ ch05/ch05_2/src/pages/CardsTest/Card.tsx
@@ -0,0 +1,13 @@
+import type {FC} from 'react'
+import type {ICard} from '../../data'
+import {Div, Avatar} from '../../components'
+import {Icon} from '../../theme/daisyui'
+
+export default function Card() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
