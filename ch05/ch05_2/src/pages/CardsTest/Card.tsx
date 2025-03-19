1aba35abd9d8590b5421f4411a8cadf7b9f181b2
yisj777s <yisj777s@naver.com>
2025-03-19 20:55:20 +0900

    unfinished18 05.상태 관리와 리덕스 패키지_05-2.리듀서 활용하기

--- ch05/ch05_2/src/pages/CardsTest/Card.tsx
+++ ch05/ch05_2/src/pages/CardsTest/Card.tsx
@@ -3,7 +3,19 @@ import type {ICard} from '../../data'
 import {Div, Avatar} from '../../components'
 import {Icon} from '../../theme/daisyui'
 
-export default function Card() {
+export type UserCardProps = {
+  card: ICard
+  onRemove?: () => void
+}
+
+const Card: FC<UserCardProps> = ({card, onRemove}) => {
+  const {image, writer} = card
+  const {avatar, name, jobTitle} = writer
+
+  return <Div></Div>
+}
+
+export default function CardTest() {
   return (
     <section className="mt-4">
       <h2 className="text-5xl font-bold text-center">CopyMe</h2>
