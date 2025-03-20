1f1eb35cd536b1b8eb40d49cc55d7b43780edc0c
yisj777s <yisj777s@naver.com>
2025-03-20 21:06:47 +0900

    unfinished19 05.상태 관리와 리덕스 패키지_05-2.리듀서 활용하기

--- ch05/ch05_2/src/pages/CardsTest/Card.tsx
+++ ch05/ch05_2/src/pages/CardsTest/Card.tsx
@@ -12,7 +12,20 @@ const Card: FC<UserCardProps> = ({card, onRemove}) => {
   const {image, writer} = card
   const {avatar, name, jobTitle} = writer
 
-  return <Div></Div>
+  return (
+    <Div>
+      <Div src={image} className="relative h-20">
+        <Icon
+          name="remove"
+          className="absolute right-1 top-1 btn-primary btn-xs"
+          onClick={onRemove}
+        />
+      </Div>
+      <Div className="flex flex-col p-2">
+        <Div minHeight="4"></Div>
+      </Div>
+    </Div>
+  )
 }
 
 export default function CardTest() {
