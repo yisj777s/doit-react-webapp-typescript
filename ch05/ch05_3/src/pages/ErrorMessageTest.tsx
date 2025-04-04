e5e6093cfe971c6d8bbad3dc5d4e6bcf43d22b16
yisj777s <yisj777s@naver.com>
2025-04-04 21:32:03 +0900

    unfinished10 05.상태 관리와 리덕스 패키지_05-3.리덕스 미들웨어 이해하기

--- ch05/ch05_3/src/pages/ErrorMessageTest.tsx
+++ ch05/ch05_3/src/pages/ErrorMessageTest.tsx
@@ -1,8 +1,37 @@
-export default function CopyMe() {
+import {useCallback} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import type {AppState} from '../store'
+import {Title} from '../components'
+import {Button} from '../theme/daisyui'
+import * as D from '../data'
+import * as E from '../store/errorMessage'
+
+export default function ErrorMessageTest() {
+  const dispatch = useDispatch()
+  const errorMessage = useSelector<AppState, E.State>(({errorMessage}) => errorMessage)
+
+  const generateErrorMessage = useCallback(() => {
+    dispatch<any>(E.generateErrorMessage(D.randomSentence()))
+  }, [dispatch])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>Loading</Title>
+      <div className="mt-4">
+        <div className="flex justify-center mt-4">
+          <Button 
+            className="btn-sm btn-soft btn-secondary" 
+            onClick={doTimedLoading} 
+            disabled={loading}>
+            DO TIMED LOADING
+          </Button>
+        </div>
+        {loading && (
+          <div className="flex items-center justify-center">
+            <Button className="btn-circle loading"></Button>
+          </div>
+        )}
+      </div>
     </section>
   )
 }
