9b5e84b8667d84ea2b75a708835ad28e75961278
yisj777s <yisj777s@naver.com>
2025-04-22 21:26:23 +0900

    unfinished08 05.상태 관리와 리덕스 패키지_05-4.트렐로 따라 만들기

--- ch05/ch05_4/src/store/listidOrders/types.ts
+++ ch05/ch05_4/src/store/listidOrders/types.ts
@@ -7,3 +7,11 @@ export type State = UUID[]
 export type SetListidOrders = Action<'@listidOrders/set'> & {
   payload: State
 }
+export type AddListidToOrders = Action<'@listidOrders/add'> & {
+  payload: UUID
+}
+export type RemoveListidFromOrders = Action<'@listidOrders/remove'> & {
+  payload: UUID
+}
+
+export type Actions = SetListidOrders | AddListidToOrders | RemoveListidFromOrders
