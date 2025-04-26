fd17b1d88ec4995fa1c4a3ea12b9ce0f2534823a
yisj777s <yisj777s@naver.com>
2025-04-26 21:25:31 +0900

    unfinished11 05.상태 관리와 리덕스 패키지_05-4.트렐로 따라 만들기

--- ch05/ch05_4/src/pages/Board/index.tsx
+++ ch05/ch05_4/src/pages/Board/index.tsx
@@ -2,15 +2,29 @@ import {useCallback} from 'react'
 import {useDispatch} from 'react-redux'
 import {Title} from '../../components'
 import CreateListForm from './CreateListForm'
-
+import BoardList from '../BoardList'
+import type {AppState} from '../../store'
+import type {List} from '../../store/commonTypes'
 import * as LO from '../../store/listidOrders'
 import * as L from '../../store/listEntities'
 
 export default function Board() {
   const dispatch = useDispatch()
 
-  const onCreateList = useCallback((uuid: string, title: string) => {
-    const list = {uuid, titile}
-    dispatch(LO.addlistid)
-  })
+  const onCreateList = useCallback(
+    (uuid: string, title: string) => {
+      const list = {uuid, title}
+      dispatch(LO.addListidToOrders(list.uuid))
+      dispatch(L.addList(list))
+    },
+    [dispatch]
+  )
+  return (
+    <section className="mt-4">
+      <Title>Board</Title>
+      <div>
+        <CreateListForm onCreateList={onCreateList} />
+      </div>
+    </section>
+  )
 }
--- ch05/ch05_4/src/pages/BoardList/index.tsx
+++ ch05/ch05_4/src/pages/BoardList/index.tsx
@@ -1,8 +1,21 @@
-export default function CopyMe() {
+import type {FC} from 'react'
+import type {List} from '../../store/commonTypes'
+import {Icon} from '../../theme/daisyui'
+
+export type BoardListProps = {
+  list: List
+  onRemoveList?: () => void
+}
+const BoardList: FC<BoardListProps> = ({list, onRemoveList, ...props}) => {
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
+    <div {...props} className="p-2 m-2 border border-gray-300 rounded-lg">
+      <div className="flex justify-between mb-2">
+        <p className="w-32 text-sm font-bold uderline line-clamp-1">{list.title}</p>
+        <div className="flex justify-between ml-2">
+          <Icon name="remove" className="btn-error btn-xs" onClick={onRemoveList} />
+        </div>
+      </div>
+    </div>
   )
 }
+export default BoardList
