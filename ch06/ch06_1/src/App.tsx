4f30af7ead63d7fe485a3af743f294cb4e2d39eb
yisj777s <yisj777s@naver.com>
2025-06-02 21:01:57 +0900

    unfinished02 06.리액트 라우터_06-1.처음 만나는 리액트 라우터

--- ch06/ch06_1/src/App.tsx
+++ ch06/ch06_1/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Provider as ReduxProvider} from 'react-redux'
+import {DndProvider} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
+import {useStore} from './store'
+import Board from './pages/Board'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const store = useStore()
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ReduxProvider store={store}>
+      <DndProvider backend={HTML5Backend}>
+        <Board />
+      </DndProvider>
+    </ReduxProvider>
   )
 }
-
-export default App
