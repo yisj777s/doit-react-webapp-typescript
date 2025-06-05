b90af455a00c7796ff9fe2aaef077579037ade73
yisj777s <yisj777s@naver.com>
2025-06-05 21:19:25 +0900

    unfinished04 06.리액트 라우터_06-1.처음 만나는 리액트 라우터

--- ch06/ch06_1/src/routes/RoutesSetup.tsx
+++ ch06/ch06_1/src/routes/RoutesSetup.tsx
@@ -1,12 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import NoMatch from './NoMatch'
 import Home from './Home'
+import Board from '../pages/Board'
 
 export default function RoutesSetup() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/welcome" element={<Home title="Welcome to our site" />} />
+      <Route path="/board" element={<Board />} />
       <Route path="*" element={<NoMatch />} />
     </Routes>
   )
