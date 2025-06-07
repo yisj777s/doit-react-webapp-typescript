70425952b931ed607d38bf375cdb8a739cf502eb
yisj777s <yisj777s@naver.com>
2025-06-07 21:10:04 +0900

    unfinished06 06.리액트 라우터_06-1.처음 만나는 리액트 라우터

--- ch06/ch06_1/src/routes/Card.tsx
+++ ch06/ch06_1/src/routes/Card.tsx
@@ -1,3 +1,20 @@
+import {useCallback} from 'react'
+import {useLocation, useNavigate} from 'react-router-dom'
+import {Button} from '../theme/daisyui'
+
 export default function Card() {
-  return <h1>card</h1>
+  const location = useLocation()
+
+  const navigate = useNavigate()
+  const goBack = useCallback(() => {
+    navigate(-1)
+  }, [navigate])
+  return (
+    <div>
+      <p>location: {JSON.stringify(location, null, 2)}</p>
+      <Button className="mt-4 btn-primary btn-xs" onClick={goBack}>
+        GO BACK
+      </Button>
+    </div>
+  )
 }
