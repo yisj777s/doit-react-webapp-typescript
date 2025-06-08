c873ca2eb1fe8638eb27fe48598c404e2e883c77
yisj777s <yisj777s@naver.com>
2025-06-08 21:48:55 +0900

    unfinished07 06.리액트 라우터_06-1.처음 만나는 리액트 라우터

--- ch06/ch06_1/src/routes/Card.tsx
+++ ch06/ch06_1/src/routes/Card.tsx
@@ -1,17 +1,27 @@
+import type {AppState} from '../store'
+import type {Card as CardType} from '../store/commonTypes'
+import * as CE from '../store/cardEntities'
+
 import {useCallback} from 'react'
-import {useLocation, useNavigate} from 'react-router-dom'
+import {useLocation, useParams, useNavigate, useSearchParams} from 'react-router-dom'
 import {Button} from '../theme/daisyui'
 
 export default function Card() {
   const location = useLocation()
-
+  const params = useParams()
   const navigate = useNavigate()
+  const [search] = useSearchParams()
   const goBack = useCallback(() => {
     navigate(-1)
   }, [navigate])
   return (
     <div>
       <p>location: {JSON.stringify(location, null, 2)}</p>
+      <p>params: {JSON.stringify(params, null, 2)}</p>
+      <p>cardid: {params['cardid']}</p>
+      <p>
+        from: {search.get('from')}, to: {search.get('to')}
+      </p>
       <Button className="mt-4 btn-primary btn-xs" onClick={goBack}>
         GO BACK
       </Button>
