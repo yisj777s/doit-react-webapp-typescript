70abc99919ada6b05126abaf10ee3366dd2f514d
yisj777s <yisj777s@naver.com>
2025-06-12 20:34:23 +0900

    unfinished01 06.리액트 라우터_06-2.Outlet 컴포넌트와 중첩 라우팅팅

---
+++ ch06/ch06_2/src/copy/routes/NoMatch.tsx
@@ -0,0 +1,21 @@
+import {useCallback} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {Button} from '../../theme/daisyui'
+
+export default function NoMatch() {
+  const navigate = useNavigate()
+
+  const goBack = useCallback(() => {
+    navigate(-1)
+  }, [navigate])
+  return (
+    <div className="flex flex-col p-4">
+      <p className="p-4 text-center tet-xl alert alert-error">Oops! No page found!</p>
+      <div className="flex justify-center mt-4">
+        <Button className="ml-4 btn-primary btn-xs" onClick={goBack}>
+          GO BACK
+        </Button>
+      </div>
+    </div>
+  )
+}
---
+++ ch06/ch06_2/src/routes/LandigPage/index.tsx
@@ -0,0 +1,8 @@
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
