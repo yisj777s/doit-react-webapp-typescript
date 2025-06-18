0e97252e6f9bd1ffc68d6e39e3164f9b280c2cf9
yisj777s <yisj777s@naver.com>
2025-06-18 12:18:07 +0900

    unfinished00 06.리액트 라우터_06-3.공개 라우트와 비공개 라우트 구현하기

---
+++ ch06/ch06_3/src/routes/Layout/NavigationBar.tsx
@@ -0,0 +1,13 @@
+// import {Link} from 'react-router-dom'
+import {Link} from '../../components'
+
+export default function NavigationBar() {
+  return (
+    <div className="flex p-2 navbar bg-base-100">
+      <Link to="/">Home</Link>
+      <Link to="/board" className="ml-4">
+        Board
+      </Link>
+    </div>
+  )
+}
