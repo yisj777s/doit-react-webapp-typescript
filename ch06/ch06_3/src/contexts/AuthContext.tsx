684652418395494927fdcbee5576472182031642
yisj777s <yisj777s@naver.com>
2025-06-19 19:54:13 +0900

    unfinished02 06.리액트 라우터_06-3.공개 라우트와 비공개 라우트 구현하기

--- ch06/ch06_3/src/contexts/AuthContext.tsx
+++ ch06/ch06_3/src/contexts/AuthContext.tsx
@@ -20,5 +20,30 @@ export const AuthContext = createContext<ContextType>({
 type AuthProviderProps = {}
 
 export const AuthProvider: FC<PropsWithChildren<AuthProviderProps>> = ({children}) => {
-  const [loggedUser]
+  const [loggedUser, setLoggedUser] = useState<LoggedUser | undefined>(undefined)
+
+  const signup = useCallback((email: string, password: string, callback?: Callback) => {
+    setLoggedUser(notUsed => ({email, password}))
+    callback && callback()
+  }, [])
+  const login = useCallback((email: string, password: string, callback?: Callback) => {
+    setLoggedUser(notUsed => ({email, password}))
+    callback && callback()
+  }, [])
+  const logout = useCallback((email: string, password: string, callback?: Callback) => {
+    setLoggedUser(notUsed => ({email, password}))
+    callback && callback()
+  }, [])
+
+  const value = {
+    loggedUser,
+    signup,
+    login,
+    logout
+  }
+  return <AuthContext.Provider value={value} children={children} />
+}
+
+export const useAuth = () => {
+  return useContext(AuthContext)
 }
--- ch06/ch06_3/src/routes/RoutesSetup.tsx
+++ ch06/ch06_3/src/routes/RoutesSetup.tsx
@@ -2,6 +2,9 @@ import {Routes, Route} from 'react-router-dom'
 import Layout from './Layout'
 import LandingPage from './LandingPage'
 import Board from '../pages/Board'
+import Signup from './Auth/SignUp'
+import Login from './Auth/Login'
+import Logout from './Auth/Logout'
 import NoMatch from './NoMatch'
 
 export default function RoutesSetup() {
@@ -12,6 +15,10 @@ export default function RoutesSetup() {
         <Route path="/board" element={<Board />} />
         <Route path="*" element={<NoMatch />} />
       </Route>
+      <Route path="/sigup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="*" element={<NoMatch />} />
     </Routes>
   )
 }
