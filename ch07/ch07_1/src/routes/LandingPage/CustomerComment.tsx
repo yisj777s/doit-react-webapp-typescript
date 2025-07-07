d189b73cf5130c141cbb0ecc3608787fb919dfbc
yisj777s <yisj777s@naver.com>
2025-07-07 12:27:15 +0900

    unfinished00 07.몽고DB와 API서버_07-1.몽고DB 이해하기

---
+++ ch07/ch07_1/src/routes/LandingPage/CustomerComment.tsx
@@ -0,0 +1,30 @@
+import type {FC} from 'react'
+import type {CustomerComment as CustomerCommentType} from '../../data'
+import {Div, Avatar} from '../../components'
+
+export type CustomerCommentProps = {
+  customerComment: CustomerCommentType
+}
+const CustomerComment: FC<CustomerCommentProps> = ({customerComment}) => {
+  const {name, jobTitle, company, avatar, comment} = customerComment
+  return (
+    <Div
+      className="relative p-2 mx-2 mt-8 border-2 rounded-lg shadow-lg border-primary"
+      minWidth="20rem"
+      width="20rem"
+      minHeight="10rem">
+      <div className="absolute flex items-center w-full jsutify-center -top-7">
+        <Avatar src={avatar} className="border-2 border-primary" />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-col p-4 font-bold">
+          <p>{name}</p>
+          <p>{jobTitle}</p>
+          <p>{company}</p>
+        </div>
+        <p className="mt-4">{comment}</p>
+      </div>
+    </Div>
+  )
+}
+export default CustomerComment
---
+++ ch07/ch07_1/src/routes/Layout/NavigationBar.tsx
@@ -0,0 +1,39 @@
+import {Link as RRLink} from 'react-router-dom'
+import {Link} from '../../components'
+import {useAuth} from '../../contexts'
+
+export default function NavigationBar() {
+  const {loggedUser} = useAuth()
+
+  return (
+    <div className="flex justify-between bg-base-100">
+      <div className="flex p-2 navbar">
+        <Link to="/" className="btn btn-link">
+          Home
+        </Link>
+        {loggedUser && (
+          <Link to="/board" className="ml-4 btn btn-link">
+            Board
+          </Link>
+        )}
+      </div>
+      <div className="flex items-center p-2">
+        {!loggedUser && (
+          <RRLink to="/login" className="btn btn-sm btn-primary">
+            LOGIN
+          </RRLink>
+        )}
+        {!loggedUser && (
+          <RRLink to="/signup" className="ml-4 btn btn-sm btn-outline btn-primary">
+            SIGNUP
+          </RRLink>
+        )}
+        {loggedUser && (
+          <RRLink to="/logout" className="ml-4 mr-4 btn btn-sm btn-link">
+            LOGOUT
+          </RRLink>
+        )}
+      </div>
+    </div>
+  )
+}
