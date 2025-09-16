1a0cecdbdba7be51a76220c4a535d2c9dcfe08bb
yisj777s <yisj777s@naver.com>
2025-09-16 20:10:13 +0900

    unfinished06 07.몽고DB와 API서버_07-3.익스프레스 프레임워크로 API 서버 만들기

--- ch07/ch07_3_server/src/express/index.ts
+++ ch07/ch07_3_server/src/express/index.ts
@@ -1,13 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 
 export const createExpressApp = (...args: any[]) => {
   const app = express()
+
   app
     .use((req, res, next) => {
       console.log(`url='${req.url}, method=${req.method}`)
       next()
     })
     .use(express.static('public'))
+    .use(express.json())
+    .use(cors())
     .get('/', (req, res) => {
       res.json({message: 'Hello express World!'})
     })
