a0abffd8c0488e273c083d8889ea7adaf63aa7fb
yisj777s <yisj777s@naver.com>
2025-10-01 20:32:40 +0900

    unfinished14 07.몽고DB와 API서버_07-3.익스프레스 프레임워크로 API 서버 만들기

--- ch07/ch07_3_client/src/App.tsx
+++ ch07/ch07_3_client/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Provider as ReduxProvider} from 'react-redux'
+import {DndProvider} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
+import {BrowserRouter} from 'react-router-dom'
+import {AuthProvider} from './contexts'
+import RoutesSetup from './routes/RoutesSetup'
+import {useStore} from './store'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const store = useStore()
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ReduxProvider store={store}>
+      <DndProvider backend={HTML5Backend}>
+        <BrowserRouter>
+          <AuthProvider>
+            <RoutesSetup />
+          </AuthProvider>
+        </BrowserRouter>
+      </DndProvider>
+    </ReduxProvider>
   )
 }
-
-export default App
