d6bd5158d82a1a200b9ad9b0d2d1ec22c51a567b
yisj777s <yisj777s@naver.com>
2025-10-09 19:30:49 +0900

    unfinished17 07.몽고DB와 API서버_07-3.익스프레스 프레임워크로 API 서버 만들기

--- ch07/ch07_3_client/src/routes/RestTest/GetTest.tsx
+++ ch07/ch07_3_client/src/routes/RestTest/GetTest.tsx
@@ -1,8 +1,20 @@
-export default function CopyMe() {
+import {useState, useEffect} from 'react'
+
+export default function GetTest() {
+  const [data, setData] = useState<object>({})
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:5173/test')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(error => setErrorMessage(error.message))
+  }, [])
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
+    <div>
+      <p className="text-2xl text-center text-bold">GetTest</p>
       <div className="mt-4"></div>
-    </section>
+    </div>
   )
 }
