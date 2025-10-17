3e874f906f9dd5f5e45090bd118ab2dc4c730565
yisj777s <yisj777s@naver.com>
2025-10-17 20:51:30 +0900

    unfinishe21 07.몽고DB와 API서버_07-3.익스프레스 프레임워크로 API 서버 만들기

--- ch07/ch07_3_client/src/routes/RestTest/GetTest.tsx
+++ ch07/ch07_3_client/src/routes/RestTest/GetTest.tsx
@@ -1,19 +1,34 @@
-import {useState, useEffect} from 'react'
+import {useState, useCallback} from 'react'
+import {Button} from '../../theme/daisyui'
+import {get} from '../../server'
 
 export default function GetTest() {
   const [data, setData] = useState<object>({})
   const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
-  useEffect(() => {
-    fetch('http://localhost:4000/test')
+  const getAllTest = useCallback(() => {
+    get('/test')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(error => setErrorMessage(error.message))
+  }, [])
+  const getTest = useCallback(() => {
+    get('/test/1234')
       .then(res => res.json())
       .then(data => setData(data))
       .catch(error => setErrorMessage(error.message))
   }, [])
 
   return (
-    <div>
-      <p className="text-2xl text-center text-bold">GetTest</p>
+    <div className="mb-4">
+      <div className="flex justify-center mb-4">
+        <Button onClick={getAllTest} className="mr-12 btn-primary">
+          GET ALL
+        </Button>
+        <Button onClick={getTest} className="btn-primary">
+          GET ID 1234
+        </Button>
+      </div>
       <div className="mt-4 text-center">
         <p>data: {JSON.stringify(data, null, 2)}</p>
         {errorMessage && <p>error: {errorMessage}</p>}
