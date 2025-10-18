db65dcb8e69c71a51f57ccd68826460b23f38bf3
yisj777s <yisj777s@naver.com>
2025-10-18 20:27:10 +0900

    unfinishe22 07.몽고DB와 API서버_07-3.익스프레스 프레임워크로 API 서버 만들기

--- ch07/ch07_3_client/src/routes/RestTest/PostTest.tsx
+++ ch07/ch07_3_client/src/routes/RestTest/PostTest.tsx
@@ -1,8 +1,11 @@
-export default function CopyMe() {
-  return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
-  )
+import {useState, useCallback} from 'react'
+import {Button} from '../../theme/daisyui'
+import {post} from '../../server'
+import * as D from '../../data'
+
+export default function PostTest() {
+  const [data, setData] = useState<object>({})
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  const postTest = useCallback(() => {})
 }
