1135e77402026854d4b29a748e5781e83a1e1515
yisj777s <yisj777s@naver.com>
2025-10-21 20:22:13 +0900

    unfinishe23 07.몽고DB와 API서버_07-3.익스프레스 프레임워크로 API 서버 만들기

--- ch07/ch07_3_client/src/routes/RestTest/PutTest.tsx
+++ ch07/ch07_3_client/src/routes/RestTest/PutTest.tsx
@@ -1,8 +1,2 @@
-export default function CopyMe() {
-  return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
-  )
-}
+import {useState, useCallback} from 'react'
+import {put} from '../../server'
