dc8c5bd7693b6bee6f71852cfa9a8efd62fb41f8
yisj777s <yisj777s@naver.com>
2025-10-22 20:51:22 +0900

    unfinishe24 07.몽고DB와 API서버_07-3.익스프레스 프레임워크로 API 서버 만들기

--- ch07/ch07_3_client/src/routes/RestTest/PutTest.tsx
+++ ch07/ch07_3_client/src/routes/RestTest/PutTest.tsx
@@ -1,2 +1,30 @@
 import {useState, useCallback} from 'react'
 import {put} from '../../server'
+import * as D from '../../data'
+import {Button} from '../../theme/daisyui'
+
+type Body = Record<'id' | string, any>
+type Data = {
+  ok: boolean
+  body?: Body
+  errorMessage?: string
+}
+export default function PutTest() {
+  const [data, setData] = useState<Data | null>(null)
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+  const putTest = useCallback(() => {
+    put('/test/1234', D.makeRandomCard())
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(error => setErrorMessage(error.message))
+  }, [])
+
+  return (
+    <div>
+      <div className="mb-4">
+        <div></div>
+      </div>
+      <div className="mt-4 text-center"></div>
+    </div>
+  )
+}
