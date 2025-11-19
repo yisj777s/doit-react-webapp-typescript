852e148f82baad3204ff64cb4c276ce8b857d55d
yisj777s <yisj777s@naver.com>
2025-11-19 19:45:16 +0900

    unfinishe09 07.몽고DB와 API서버_07-4.JSON 웹 토큰으로 회원 인증 기능 구현하기

---
+++ ch07/ch07_4_server/src/routers/authRouter.ts
@@ -0,0 +1,27 @@
+import type {MongoDB} from '../mongodb'
+import {Router} from 'express'
+import * as U from '../utils'
+
+export const authRouter = (...args: any[]) => {
+  const db: MongoDB = args[0]
+  const user = db.collection('user')
+  const router = Router()
+
+  return router.post('/signUP', async (req, res) => {
+    const {body} = req
+
+    try {
+      // console.log('./signup', body)
+      const exists = await user.findOne({email: body.email})
+
+      if (exists) {
+        res.json({ok: false, errorMessage: '이미 가입한 회원입니다.'})
+      } else {
+        const {email, password} = body
+        const hashed = await U.hashPasswordP(password)
+        const newBody = {email, password: hashed}
+        const {insertedId}
+      }
+    }
+  })
+}
