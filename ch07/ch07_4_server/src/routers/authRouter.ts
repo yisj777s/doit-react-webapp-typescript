fdf435f86774fa9c2e2916b0e051aec142e05179
yisj777s <yisj777s@naver.com>
2025-12-03 20:35:18 +0900

    unfinishe16 07.몽고DB와 API서버_07-4.JSON 웹 토큰으로 회원 인증 기능 구현하기

--- ch07/ch07_4_server/src/routers/authRouter.ts
+++ ch07/ch07_4_server/src/routers/authRouter.ts
@@ -1,4 +1,5 @@
 import type {MongoDB} from '../mongodb'
+import {stringToObjectId} from '../mongodb'
 import {Router} from 'express'
 import * as U from '../utils'
 
@@ -7,26 +8,34 @@ export const authRouter = (...args: any[]) => {
   const user = db.collection('user')
   const router = Router()
 
-  return router.post('/signup', async (req, res) => {
-    const {body} = req
+  return router
+    .post('/signup', async (req, res) => {
+      const {body} = req
 
-    try {
-      // console.log('./signup', body)
-      const exists = await user.findOne({email: body.email})
+      try {
+        // console.log('./signup', body)
+        const exists = await user.findOne({email: body.email})
 
-      if (exists) {
-        res.json({ok: false, errorMessage: '이미 가입한 회원입니다.'})
-      } else {
-        const {email, password} = body
-        const hashed = await U.hashPasswordP(password)
-        const newBody = {email, password: hashed}
-        const {insertedId} = await user.insertOne(newBody)
-        const jwt = await U.jwtSignP({userId: insertedId})
+        if (exists) {
+          res.json({ok: false, errorMessage: '이미 가입한 회원입니다.'})
+        } else {
+          const {email, password} = body
+          const hashed = await U.hashPasswordP(password)
+          const newBody = {email, password: hashed}
+          const {insertedId} = await user.insertOne(newBody)
+          const jwt = await U.jwtSignP({userId: insertedId})
 
-        res.json({ok: true, body: jwt})
+          res.json({ok: true, body: jwt})
+        }
+      } catch (e) {
+        if (e instanceof Error) res.json({ok: false, errorMessage: e.message})
       }
-    } catch (e) {
-      if (e instanceof Error) res.json({ok: false, errorMessage: e.message})
-    }
-  })
+    })
+    .post('/login', async (req, res) => {
+      const {authorization} = req.headers || {}
+      if (!authorization) {
+        res.json({ok: false, errorMessage: 'JSON 토큰이 없습니다.'})
+        return
+      }
+    })
 }
